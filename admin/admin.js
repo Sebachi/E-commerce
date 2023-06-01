60e9e8d2305b6e0668367cf3b95a6273599994be
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-06-01 16:41:58 -0500

    Adding admin pages

---
+++ admin/admin.js
@@ -0,0 +1,212 @@
+// Reponsive Section
+const hamburguesa = document.querySelector(".hamburguesa");
+const burgerIco = document.getElementById("burgerIco");
+const menuMobile = document.querySelector(".ulMobile");
+let isOpen = false;
+hamburguesa.addEventListener("click", (e) => {
+  menuMobile.classList.toggle("inactive");
+  if (isOpen) {
+    burgerIco.src = "resources/icon-hamburger.svg";
+  } else {
+    burgerIco.src = "resources/icon-close.svg";
+  }
+  isOpen = !isOpen;
+});
+
+//Declaramos elementos del DOM para manipularlos
+
+const btnNewProduct = document.querySelector(".nav__item");
+const formNewProduct = document.getElementById("formNewProduct");
+const URL_API = "http://localhost:4000/stockFItems";
+const allProductsContainer = document.getElementById("allProductsContainer");
+const inputName = document.querySelector("#name");
+const inputPrice = document.querySelector("#price");
+const inputAmount = document.querySelector("#amount");
+const inputWeight = document.querySelector("#weight");
+const inputType = document.querySelector("#type");
+const inputImage = document.querySelector("#image");
+const titleForm = document.querySelector(".subtitle");
+const btnForm = document.querySelector(".form__btn");
+
+//Codigo
+let isEdit = false;
+let idProductEdit;
+
+const handleToggleForm = () => {
+  formNewProduct.classList.remove("hidden");
+  titleForm.innerHTML = "Nuevo producto";
+  btnForm.innerHTML = "Save";
+  formNewProduct.reset();
+  isEdit = false;
+};
+//Metodo Get
+const getAdmin = async () => {
+  const response = await fetch(URL_API);
+  const data = await response.json();
+  allProductsContainer.innerHTML = "";
+  data.forEach((element) => {
+    allProductsContainer.innerHTML += `
+    <div id="${element.id}" class="productContainer">
+      <figure class="productFigure">
+       <img src="${element.itemImage}" class="" alt="${element.itemName}" />
+      </figure>
+      <div class="productInfo">
+        <p class="productType">${element.itemType}</p>
+        <h3 class="productName">${element.itemName}</h3>
+        <p class="productWeight">${element.itemWeight}</p>
+        <p class="productPrice">${element.itemPrice}</p>
+        <span class="counter" id="counter${element.itemAmount}">Cantidad actual: ${element.itemAmount}</span>
+      </div>
+      <div class="edit_delete">
+          <button class="operation edit" id="edit${element.id}">
+          ✏️
+          </button>
+          <button class="operation delete" id="delete${element.id}">
+          ❌
+          </button>
+        </div>
+      </div>
+
+      `;
+  });
+};
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await getAdmin();
+  const productContainer = document.querySelectorAll(".productContainer");
+  productContainer.forEach((element) => {
+    const localProduct = element.getAttribute("id");
+    const btnEdit = document.getElementById(`edit${localProduct}`);
+    const btnDelete = document.getElementById(`delete${localProduct}`);
+
+
+ 
+
+
+
+    btnEdit.addEventListener("click", () => {
+      console.log('he recibido el edit');
+      handleEdit(localProduct)
+    });
+    btnDelete.addEventListener("click", () => {
+      handleDelete(localProduct)
+      console.log('he recibido el delete');
+     
+    });
+
+  });
+
+
+});
+
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  if (!inputName.value || !inputPrice.value || !inputAmount.value || !inputWeight.value || !inputImage.value || !inputType.value) {
+      alert('hay campos obligatorios por llenar')
+      return
+  }
+  const newProduct = {
+      itemName: inputName.value,
+      itemPrice: Number(inputPrice.value),
+      itemAmount: Number(inputAmount.value),
+      itemWeight: inputWeight.value,
+      itemType: inputType.value,
+      itemImage: inputImage.value,
+  }
+  if (isEdit) {
+      const response = await updateProduct(newProduct);
+      if(response.status == 200) {
+          alert('Producto actualizado exitosamente')
+      } else {
+          alert('Hubo un error al actualizar producto')
+      }
+  }else {
+      const response = await saveProduct(newProduct);
+      if(response.status == 201) {
+          alert('Producto guardado exitosamente')
+      } else {
+          alert('Hubo un error al guardar producto')
+      }
+  }
+  formNewProduct.reset()
+  formNewProduct.classList.add('hidden')
+  location.reload()
+  getAdmin()
+}
+
+
+
+    const updateProduct = async (product) => {
+      try {
+          const response = await fetch(`${URL_API}/${idProductEdit}`, {
+              method: 'PATCH',
+              body: JSON.stringify(product),
+              headers: {
+                  "Content-type": "application/json"
+              }
+          })
+          return response;
+      } catch (error) {
+          console.log(error);
+          return error;
+      }
+  }
+  
+  const saveProduct = async (product) => {
+      try {
+          const response = await fetch(URL_API, {
+              method: 'POST',
+              body: JSON.stringify(product),
+              headers: {
+                  "Content-type": "application/json"
+              }
+          })
+          return response
+      } catch(error) {
+          console.log(error)
+          return error
+      }
+  }
+  
+
+
+    const handleEdit = async (localProduct) => {
+      isEdit = true;
+      idProductEdit = localProduct;
+      //Show the form for editing
+      formNewProduct.classList.remove('hidden');
+      //Get the product info
+      const response = await fetch(`${URL_API}/${localProduct}`);
+      const product = await response.json();
+      console.log(product)
+      //Fill the form with the product
+      inputName.value = product.itemName;
+      inputPrice.value = product.itemPrice;
+      inputAmount.value = product.itemAmount;
+      inputWeight.value = product.itemWeight;
+      inputType.value = product.itemType;
+      inputImage.value = product.inputImage;
+      titleForm.innerHTML = 'Actualizar producto';
+      btnForm.innerHTML = 'Actualizar';
+  }
+
+  const handleDelete = async (localProduct) => {
+    console.log(localProduct)
+    const response = await fetch(`${URL_API}/${localProduct}`, {
+        method: 'DELETE',
+    });
+    if(response.status === 200) {
+        alert('Producto eliminado exitosamente')
+    } else {
+        alert('Hubo un error al eliminar productos')
+    }
+    location.reload()
+    getAdmin()
+  }
+
+  btnNewProduct.addEventListener('click', handleToggleForm)
+
+formNewProduct.addEventListener('submit', (event)=> {
+    handleSubmit(event) })
--- wishlist/wishlist.js
+++ wishlist/wishlist.js
@@ -9,13 +9,13 @@ const getWishlist = async () => {
     console.log(newArray);
     data.forEach((element) => {
       newArray.forEach((newArrayelement) => {
-        if (newArrayelement == element.itemId) {
+        if (newArrayelement == element.id) {
           wishlistContainer.innerHTML += `
-                      <div id="${element.itemId}" class="productContainer">
+                      <div id="${element.id}" class="productContainer">
                           <figure class="productFigure">
                       <img src="${element.itemImage}" class="" alt="${element.itemName}">
                            </figure>
-                          <button class="wishIcon activated" id="wishIcon${element.itemId}"><svg fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="rotate(-45)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>heart</title> <path d="M0.256 12.16q0.544 2.080 2.080 3.616l13.664 14.144 13.664-14.144q1.536-1.536 2.080-3.616t0-4.128-2.080-3.584-3.584-2.080-4.16 0-3.584 2.080l-2.336 2.816-2.336-2.816q-1.536-1.536-3.584-2.080t-4.128 0-3.616 2.080-2.080 3.584 0 4.128z"></path> </g></svg>
+                          <button class="wishIcon activated" id="wishIcon${element.id}"><svg fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="rotate(-45)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>heart</title> <path d="M0.256 12.16q0.544 2.080 2.080 3.616l13.664 14.144 13.664-14.144q1.536-1.536 2.080-3.616t0-4.128-2.080-3.584-3.584-2.080-4.16 0-3.584 2.080l-2.336 2.816-2.336-2.816q-1.536-1.536-3.584-2.080t-4.128 0-3.616 2.080-2.080 3.584 0 4.128z"></path> </g></svg>
                            </button>
                               <div class="productInfo">
                         <p class="productType">${element.itemType}</p>
@@ -24,9 +24,9 @@ const getWishlist = async () => {
                          <p class="productPrice">${element.itemPrice}</p>
                            </div>
             <div class="qty_Modifier">
-            <button class="operation minus" id="minus${element.itemId}"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#000000"></path> <path d="M8 12H16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
-            <span class="counter" id="counter${element.itemId}">0</span>
-            <button class="operation plus" id="plus${element.itemId}"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#000000"></path> <path d="M12 7V17M7 12H17M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+            <button class="operation minus" id="minus${element.id}"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#000000"></path> <path d="M8 12H16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+            <span class="counter" id="counter${element.id}">0</span>
+            <button class="operation plus" id="plus${element.id}"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#000000"></path> <path d="M12 7V17M7 12H17M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
              </div>
           </div>
             `;
