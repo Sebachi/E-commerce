8ef66f096ef31c3a7f69428db5cbafa507c90dd6
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-05-31 22:36:57 -0500

    Many changes

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ const burgerIco = document.getElementById('burgerIco');
 const menuMobile = document.querySelector('.ulMobile');
 let isOpen = false;
 hamburguesa.addEventListener('click', (e) => {
-    menuMobile.classList.toggle('inactive');
-    if (isOpen) {
-        burgerIco.src = 'resources/icon-hamburger.svg';
-    } else {
-        burgerIco.src = 'resources/icon-close.svg';
-    }
-    isOpen = !isOpen;
+  menuMobile.classList.toggle('inactive');
+  if (isOpen) {
+    burgerIco.src = 'resources/icon-hamburger.svg';
+  } else {
+    burgerIco.src = 'resources/icon-close.svg';
+  }
+  isOpen = !isOpen;
 });
 
 //Declaramos elementos del DOM para manipularlos
@@ -21,38 +21,75 @@ const category4 = document.getElementById("categories__4");
 const category5 = document.getElementById("categories__5");
 const category6 = document.getElementById("categories__6");
 const category7 = document.getElementById("categories__7");
-const TopSaveToday = document.getElementById("TopSaveToday");
-
-//elimentos aun no existentes
+const topSaveToday = document.getElementById("TopSaveToday");
+const URL_LOCAL = "http://localhost:4000/stockFItems";
+const CardProductsList = document.getElementsByClassName("CardProductsList");
 let mainContainer; //se utilizara para posicionar
 const headerProductslist = document.getElementsByClassName("headerProductslist")
-//lista de todas las categorias 0-6
-const CardProductsList = document.getElementsByClassName("CardProductsList");
 
-//const productContainer = document.getElementsByClassName("productContainer");
+const getProducts = async () => {
+  try {
+    const response = await fetch(URL_LOCAL);
+    const data = await response.json();
+    data.forEach(element => {
+      if (element.itemType == 'Vegetables' || element.itemType == "Fruits") {
+        mainContainer = CardProductsList[0]
+      }
+      else if (element.itemType == "Beverages") {
+        mainContainer = CardProductsList[1];
+      }
+      else if (element.itemType == "Meats" || element.itemType == "Fish") {
+        mainContainer = CardProductsList[2];
+      }
+      else if (element.itemType == "Frozen Foods") {
+        mainContainer = CardProductsList[3];
+      }
+      else if (element.itemType == "Biscuits" || element.itemType == "Snacks") {
+        mainContainer = CardProductsList[4];
+      }
+      else if (element.itemType == "Staples") {
+        mainContainer = CardProductsList[5];
+      }
+      else { mainContainer = CardProductsList[6]; }
 
-//eventos
-
-document.addEventListener('DOMContentLoaded', (e) => {
-  descuento();
-  demasCategorias();
-  handleCreateArticle();
-})
+      mainContainer.innerHTML += `
+    <div id="${element.itemId}" class="productContainer">
+      <figure class="productFigure">
+        <img src="${element.itemImage}" class="" alt="${element.itemName}">
+      </figure>
+      <button class="wishIcon" id="wishIcon${element.itemId}"><svg fill="#000000" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="rotate(-45)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>heart</title> <path d="M0.256 12.16q0.544 2.080 2.080 3.616l13.664 14.144 13.664-14.144q1.536-1.536 2.080-3.616t0-4.128-2.080-3.584-3.584-2.080-4.16 0-3.584 2.080l-2.336 2.816-2.336-2.816q-1.536-1.536-3.584-2.080t-4.128 0-3.616 2.080-2.080 3.584 0 4.128z"></path> </g></svg>
+      </button>
+      <div class="productInfo">
+        <p class="productType">${element.itemType}</p>
+        <h3 class="productName">${element.itemName}</h3>
+        <p class="productWeight">${element.itemWeight}</p>
+        <p class="productPrice">${element.itemPrice}</p>
+      </div>
+      <div class="qty_Modifier">
+        <button class="operation plus" id="plus${element.itemId}"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#000000"></path> <path d="M12 7V17M7 12H17M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+        <span class="counter" id="counter${element.itemId}">0</span>
+        <button class="operation minus" id="minus${element.itemId}"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#000000"></path> <path d="M8 12H16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+      </div>
+    </div>
+      `
+    })
+    return response
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
 
 //mostrar primera categoria
 category1.addEventListener('click', (e) => {
-  console.log("me undiste 1");
   e.preventDefault();
   handleToggleArticle();
   CardProductsList[0].classList.remove("hidden");
-  console.log("funciono 1");
 });
 category2.addEventListener('click', (e) => {
   e.preventDefault();
   handleToggleArticle();
-
   CardProductsList[1].classList.remove("hidden");
-  console.log("me undiste 2");
 });
 category3.addEventListener('click', (e) => {
   e.preventDefault();
@@ -102,77 +139,7 @@ const handleToggleArticle = () => {
 }
 
 //crea la lista de articulos en la main page
-const handleCreateArticle = () => {
-  stockFItems.forEach(product => {
 
-    //crear elementos de las carta de productos
-    const productContainer = document.createElement("div");
-    const productFigure = document.createElement("figure");
-    const productImage = document.createElement("img");
-    const productInfo = document.createElement("div");
-    const productType = document.createElement("p");
-    const productName = document.createElement("h3");
-    const productWeight = document.createElement("p");
-    const productPrice = document.createElement("p");
-
-    //seleccionar la categoria a la que ira el producto
-    if (product.itemType == "Vegetables" || product.itemType == "Fruits") {
-      mainContainer = CardProductsList[0];
-
-    } else if (product.itemType == "Beverages") {
-      mainContainer = CardProductsList[1];
-
-    } else if (product.itemType == "Meats" || product.itemType == "Fish") {
-      mainContainer = CardProductsList[2];
-
-    } else if (product.itemType == "Frozen Foods") {
-      mainContainer = CardProductsList[3];
-
-    } else if (product.itemType == "Biscuits" || product.itemType == "Snacks") {
-      mainContainer = CardProductsList[4];
-
-    } else if (product.itemType == "Staples") {
-      mainContainer = CardProductsList[5];
-
-    } else {
-      mainContainer = CardProductsList[6];
-    }
-
-    //propiedad de su articulo principal
-
-    //darle propiedades
-    productContainer.setAttribute('id', product.itemId)
-    productImage.src = product.itemImage;
-    productImage.setAttribute('alt', product.itemName);
-    productType.innerText = product.itemType;
-    productName.innerText = product.itemName;
-    productWeight.innerText = product.itemWeight;
-    productPrice.innerText = `$ ${product.itemPrice}`;
-
-    //darle clases
-    productContainer.classList.add('productContainer')
-
-    productFigure.classList.add("productFigure");
-    productImage.classList.add("productImage");
-
-    productInfo.classList.add("productInfo");
-    productType.classList.add("productType");
-    productName.classList.add("productName");
-    productWeight.classList.add("productWeight");
-    productPrice.classList.add("productPrice");
-
-    //posicionarlos en el HTML
-    mainContainer.appendChild(productContainer)
-    productContainer.appendChild(productFigure);
-    productFigure.appendChild(productImage);
-    productContainer.appendChild(productInfo);
-    productInfo.appendChild(productType);
-    productInfo.appendChild(productName);
-    productInfo.appendChild(productWeight);
-    productInfo.appendChild(productPrice);
-  }
-  )
-};
 
 //crear la lista de descuentos
 const descuento = () => {
@@ -184,21 +151,46 @@ const descuento = () => {
 `
 };
 
-const demasCategorias = () => {
-  TopSaveToday.innerHTML += `
-  <article id="CardProductsList1" class="CardProductsList hidden">
-  </article>
-  <article id="CardProductsList2" class="CardProductsList hidden">
-  </article>
-  <article id="CardProductsList3" class="CardProductsList hidden">
-  </article>
-  <article id="CardProductsList4" class="CardProductsList hidden">
-  </article>
-  <article id="CardProductsList5" class="CardProductsList hidden">
-  </article>
-  <article id="CardProductsList6" class="CardProductsList hidden">
-  </article>
-  <article id="CardProductsList7" class="CardProductsList hidden">
-  </article>
-  `
+document.addEventListener("DOMContentLoaded", async () => {
+  await getProducts();
+  descuento();
+  const productContainer = document.querySelectorAll('.productContainer')
+  productContainer.forEach(element => {
+  const localProduct = element.getAttribute('id')
+  const btnPlus = document.getElementById(`plus${localProduct}`)
+  const btnMinus = document.getElementById(`minus${localProduct}`)
+  const counter = document.getElementById(`counter${localProduct}`)
+  const favIcon = document.getElementById(`wishIcon${localProduct}`);
+  btnPlus.addEventListener('click', () => {
+    let currentValue = Number(counter.innerText);
+    counter.innerText = currentValue + 1
+  });
+  btnMinus.addEventListener('click', () => {
+    let currentValue = Number(counter.innerText);
+    if (currentValue > 0){
+      counter.innerText = currentValue - 1
+    }
+    else {
+      counter.innerText = 0
+    }
+  });
+  favIcon.addEventListener('click', () => {
+    favIcon.classList.toggle('activated');
+  });
+});
+});
+
+
+
+
+window.saveCardElements = () => {
+
 }
+// const btnPlus = document.getElementById('plus1')
+// const btnMinus = document.getElementById('minus1')
+// const counter = document.getElementById('counter1')
+// console.log(btnPlus);
+// btnPlus.addEventListener('click', () => {
+//   let currentValue = Number(counter.innerText);
+//   counter.innerText = currentValue + 1
+// })
